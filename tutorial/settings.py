79779d3beffa9811e128df9ee6e36c2a23297583
Mr Dev <mrdev0803@gmail.com>
2022-06-04 12:41:10 -0700

    feat: complete all requirements

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1o5k)jcaof+*0@9%$1-+4vfwb@g@g&gw-2ba_c7lbj2lhaxm4o'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
